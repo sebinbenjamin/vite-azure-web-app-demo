023cf820f137e60e9fa836d9f2b7c8d0796a2c7c
Sebin Benjamin <sebinbenjamin@gmail.com>
2024-08-12 19:38:44 +1200

    feat: add weather app API in github secrets

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentTemp, setCurrentTemp] = useState();
+  const [currentCity, setCurrentCity] = useState("Auckland");
+  const [userEnteredCity, setUserEnteredCity] = useState();
+  const API_KEY= import.meta.env.VITE_WEATHER_API_KEY;
+  console.log(API_KEY);
+  
+  useEffect(() => {
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${currentCity}&aqi=no`
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response);
+        setCurrentTemp(response.current.temp_c);
+      });
+  }, [currentCity]);
+
+  const handleCityInput = (event) => {
+    setUserEnteredCity(event.target.value);
+  };
+  const handleSubmit = (e) => {
+    setCurrentCity(userEnteredCity);
+    e.preventDefault();
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Welcome to Aure !
+    <div className="App">
+      <h1>Type in your city: </h1>
+      <input onChange={handleCityInput} type="text" />
+      <button onClick={handleSubmit}>search!</button>
+      <p>
+        The temp in {currentCity} is {currentTemp && currentTemp}
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
